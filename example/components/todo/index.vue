7138a96999fa8757f651bc8c42f56a439f8eafbb
dwqs <461147874@qq.com>
2017-08-29 23:44:52 +0800

    add todo example

---
+++ example/components/todo/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="todo">
+        <h3>{{title}}:</h3>
+        <div class="list">
+            <p>
+                <input type="text" v-model='todo'>
+                <button @click="add">Add</button>
+            </p>
+            <ul v-if="all">
+                <li v-for="(item, index) in todolist" :key="index" @click="del(index)">
+                    {{item}}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    import './index.less';
+
+    export default {
+        data() {
+            return {
+                title: 'ToDo Example',
+                todo: '',
+                all: false
+            }
+        },
+
+        computed: {
+            // ...mergeProps({
+            //     all: 'todo.total',
+            //     todolist: 'todo.list'
+            // })
+        },
+
+        methods: {
+            // ...mergeActions(['todo.addTodo', 'todo.deleteTodo']),
+
+            add() {
+                if(!this.todo){
+                    return;
+                }
+                // this.addTodo({
+                //     item: this.todo
+                // });
+            },
+
+            del(index) {
+                // this.deleteTodo({
+                //     index
+                // });
+            }
+        }
+    }
+    
+</script>
