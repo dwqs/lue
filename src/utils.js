72db53de36a0d0355a4067f33e5afc353ee64739
dwqs <461147874@qq.com>
2017-08-31 01:15:18 +0800

    add some utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,19 @@
 export const assert = (condition, msg = '') => {
     if (!condition) {
-        throw new Error(`[revuejs]: ${msg}`);
+        throw new Error(`${msg}`);
     }
+};
+
+export const isObject = (obj) => {
+    return obj !== null && typeof obj === 'object';
+};
+
+export const isDef = v => v !== undefined;
+
+export const isHTMLElement = (node) => {
+    return typeof node === 'object' && node !== null && node.nodeType && node.nodeName;
+};
+
+export const forEachValue = (obj, fn) => {
+    Object.keys(obj).forEach(key => fn(obj[key], key));
 };
