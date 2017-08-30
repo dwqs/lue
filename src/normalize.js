5a517c63719c1755905978d8ded37e4fe2814c19
dwqs <461147874@qq.com>
2017-08-31 01:15:07 +0800

    add normalize.js

---
+++ src/normalize.js
@@ -0,0 +1,11 @@
+/**
+ * normalize vuex modules
+ */
+import { assert, isObject, isDef, forEachValue } from './utils';
+
+export default function normalizeModule (model, key) {
+    console.log('normalizeModulenormalizeModule', model, this);
+    let module = {};
+    //
+    return module;
+}
