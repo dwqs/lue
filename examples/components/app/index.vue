c6b6e61c6342c076c0f86aa42b7935b026fed6fe
dwqs <461147874@qq.com>
2017-09-02 02:23:52 +0800

    add app entry

---
+++ examples/components/app/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="app">
+        <h3>Examples for Revuejs: </h3>
+        <ul class="examples-list">
+            <li>
+                <router-link to='counter'>{{countTitle}}</router-link>
+            </li>
+            <li>
+                <router-link to='todo'>{{todoTitle}}</router-link>
+            </li>
+            <li>
+                <router-link to='repos'>{{reposTitle}}</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import {mergeProps} from 'lue';
+
+    export default {
+        computed: {
+            ...mergeProps({
+                'countTitle': 'counter.title',
+                'todoTitle': 'todo.title',
+                'reposTitle': 'repos.title'
+            })
+        }
+    }
+</script>
+
+<style lang="less"> 
+    .examples-list {
+        li {
+            margin-top: 10px;
+        }
+    }
+</style>
