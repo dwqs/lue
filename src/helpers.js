393290593ef432158170e18315739fcd634f7b26
dwqs <461147874@qq.com>
2017-09-02 02:26:42 +0800

    add mergeActions & mergeProps API

--- src/helpers.js
+++ src/helpers.js
@@ -1,23 +1,53 @@
 import { mapActions, mapGetters } from 'vuex';
-import { SEP, normalizeMap } from './utils';
+import { SEP, assert, isObject, forEachValue, toCamelCase } from './utils';
+import { _root } from './index';
 
 export const mergeActions = (actions) => {
-    normalizeMap(actions).forEach(({ key, val }) => {
-        const k = key.indexOf(SEP) > -1 ? key.split(SEP)[1] : key;
-        res[k] = function mappedAction (...args) {
-            return _root._namespaceActions[val].apply(_root, [].concat(args));
-        };
-    });
-    return res;
+    assert(_root, `must call new Lue() before mergeActions.`);
+    assert(isObject(actions) || Array.isArray(actions), '[lue]: the parameter of mergeActions function must be a array or plain object');
+    
+    let _validActions = {};
+
+    if (Array.isArray(actions)) {
+        actions.forEach((action) => {
+            assert(_root._namespaceActions.includes(action), `[lue]unknown action: ${action}.`);
+            let key = action.split(SEP)[1];
+            _validActions[key] = key;
+        });
+    } else {
+        forEachValue(actions, (val, key) => {
+            assert(_root._namespaceActions.includes(val), `[lue]unknown action: ${action}.`);
+            _validActions[key] = val.split(SEP)[1];
+        });
+    }
+  
+    return mapActions(_validActions);
 };
 
 export const mergeProps = (props) => {
-    normalizeMap(props).forEach(({ key, val }) => {
-        const k = key.indexOf(SEP) > -1 ? key.split(SEP)[1] : key;
-        res[k] = function mappedProps () {
-            return _root.getters[val]();
-        };
-    });
+    assert(_root, `must call new Lue() before mergeProps.`);
+    assert(isObject(props) || Array.isArray(props), '[lue]: the parameter of mergeProps function must be a array or plain object');
+    
+    let res = {};
+    let _validProps = {};
 
-    return res;
+    if (Array.isArray(props)) {
+        props.forEach((prop) => {
+            assert(_root._namespaceGetters.includes(prop), `[lue]unknown getter: ${prop}.`);
+            const namespace = prop.split(SEP)[0];
+            const getterPrefix = `get${toCamelCase(namespace)}`;
+            let key = prop.split(SEP)[1];
+            _validProps[key] = `${getterPrefix}${toCamelCase(key)}`;
+        });
+    } else {
+        forEachValue(props, (val, key) => {
+            assert(_root._namespaceGetters.includes(val), `[lue]unknown getter: ${val}`);
+            const namespace = val.split(SEP)[0];
+            const k = val.split(SEP)[1];
+            const getterPrefix = `get${toCamelCase(namespace)}`;
+            _validProps[key] = `${getterPrefix}${toCamelCase(k)}`;
+        });
+    }
+    
+    return mapGetters(_validProps);
 };
