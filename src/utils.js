281ff802a4891e3b0e1ba897f8e46a2ec142c296
dwqs <461147874@qq.com>
2017-09-02 02:29:20 +0800

    delete normalizeMap util

--- src/utils.js
+++ src/utils.js
@@ -27,9 +27,3 @@ export const toCamelCase = (str) => {
 export const isPromise = (val) => {
     return val && typeof val.then === 'function';
 };
-
-export const normalizeMap = (map) => {
-    return Array.isArray(map)
-        ? map.map(key => ({ key, val: key }))
-        : Object.keys(map).map(key => ({ key, val: map[key] }));
-};
