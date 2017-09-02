0b5da99f8d1ef9221cff9cb43fdde4502217fc33
dwqs <461147874@qq.com>
2017-09-02 21:35:15 +0800

    add helpers test

---
+++ test/actions.spec.js
@@ -0,0 +1,43 @@
+import Lue from '../src/index';
+import modules from './modules';
+
+describe('actions', () => {
+    beforeEach(() => {
+        document.body.innerHTML = window.__html__['test/index.html'];
+    });
+
+    it('counter\'s sync action', () => {
+        const app = new Lue();
+        app.createStore(modules);
+        app.initRouter({routes: []});
+        app.start('#app');
+
+        expect(app.store.state.counter.count).to.be.equal(0);
+
+        app.store.dispatch('increase');
+        app.store.dispatch('increase');
+
+        expect(app.store.state.counter.count).to.be.equal(2);
+
+        app.store.dispatch('decrease');
+        app.store.dispatch('decrease');
+
+        expect(app.store.state.counter.count).to.be.equal(0);        
+    });
+
+    it('repos\' async action', done => {
+        const app = new Lue();
+        app.createStore(modules);
+        app.initRouter({routes: []});
+        app.start('#app');
+
+        expect(app.store.state.repos.all).to.be.equal(0);
+
+        app.store.dispatch('getYourRepos', {
+            username: 'dwqs'
+        }).then(() => {
+            expect(app.store.state.repos.all).to.be.equal(11);
+            done();
+        });
+    });
+});
---
+++ test/helpers.spec.js
@@ -0,0 +1,80 @@
+import Vue from 'vue';
+
+import Lue, { mergeProps, mergeActions } from '../src/index';
+import modules from './modules';
+
+let app = null;
+
+describe('helpers', () => {
+    beforeEach(() => {
+        document.body.innerHTML = window.__html__['test/index.html'];
+        app = new Lue();
+        app.createStore(modules);
+        app.initRouter({routes: []});
+        app.start('#app');
+    });
+
+    it('mergeProps(Array)', () => {
+        const vm = new Vue({
+            store: app.store,
+            computed: mergeProps(['counter.count', 'counter.title'])
+        });
+
+        expect(vm.count).to.be.equal(modules.counter.state.count);
+        expect(vm.title).to.be.equal(modules.counter.state.title);
+    });
+
+    it('mergeProps(Object)', () => {
+        const vm = new Vue({
+            store: app.store,
+            computed: mergeProps({
+                ct: 'counter.count',
+                tl: 'counter.title'
+            })
+        });
+
+        expect(vm.ct).to.be.equal(modules.counter.state.count);
+        expect(vm.tl).to.be.equal(modules.counter.state.title);
+    });
+
+    it('mergeActions(Array)', () => {
+        const vm = new Vue({
+            store: app.store,
+            computed: mergeProps(['counter.count']),
+            methods: mergeActions(['counter.increase', 'counter.decrease'])
+        });
+
+        expect(vm.count).to.be.equal(0);
+
+        vm.increase();
+
+        expect(vm.count).to.be.equal(1);
+
+        vm.decrease();
+        vm.decrease();
+
+        expect(vm.count).to.be.equal(-1);
+    });
+
+    it('mergeActions(Object)', () => {
+        const vm = new Vue({
+            store: app.store,
+            computed: mergeProps(['counter.count']),
+            methods: mergeActions({
+                inc: 'counter.increase', 
+                dec: 'counter.decrease'
+            })
+        });
+
+        expect(vm.count).to.be.equal(-1);
+
+        vm.inc();
+
+        expect(vm.count).to.be.equal(0);
+
+        vm.dec();
+        vm.dec();
+
+        expect(vm.count).to.be.equal(-2);
+    });
+});
