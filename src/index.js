e3e1f33078102c99488a05e9f25c26c1ebe27a58
dwqs <461147874@qq.com>
2017-09-03 01:35:14 +0800

    add install methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Lue from './lue';
+import install from './install';
+
+Lue.install = install;
 
 export { mergeActions, mergeProps } from './helpers';
 export default Lue;
---
+++ src/install.js
@@ -0,0 +1,19 @@
+import { assert } from './utils';
+export let _Vue;
+
+export default function install (Vue) {
+    const version = Number(Vue.version.split('.')[0]);
+    assert(version >= 2, `[lue]: Only supports Vuejs 2 or higher.`);
+
+    if (install.installed) {
+        return;
+    }
+
+    install.installed = true;
+
+    _Vue = Vue;
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(Lue);
+}
